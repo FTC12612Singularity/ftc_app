f49cb1a799db4725be1b661776901fe96054eba2
Hillel Coates <hillelcoates@yahoo.com>
2017-11-26 13:17:20 -0800

    Added an Autonomous program. This simply drives off of the platform into the triangle zone with the loaded block. I also updated the other programs with renamed lift motors to add description.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EncoderDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EncoderDrive.java
@@ -3,17 +3,21 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous")
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Encoder Autonomous")
 //@Disabled
 public class EncoderDrive extends OpMode {
     // Declare OpMode members.
     private Robot robot = new Robot();
+    private ElapsedTime elapsedTime = new ElapsedTime();
 
     static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: NeveRest 40
     static final double DRIVE_GEAR_REDUCTION = 1.0 / 3.0;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    private int currentStep;
+    private boolean colorIsRed;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -44,13 +48,55 @@ public class EncoderDrive extends OpMode {
      */
     @Override
     public void loop() {
-        if (elapsedTime.seconds() < 1.0) {
+        switch (currentStep) {
+            case 0:
+                if (elapsedTime.seconds() < 1.0) {
+                    robot.grip(Robot.GRIPPER_STATES.GRIPPER_FULL_GRIP);
+                    telemetry.addData("Status", "Gripping");
+                } else {
+                    currentStep++;
+                }
+                break;
+            case 1:
+                if (elapsedTime.seconds() < 1.5) {
+                    robot.rightJewelServo.setPosition(robot.RIGHT_PUSH_POSITION);
+                } else if (elapsedTime.seconds() < 2.5) {
+                    colorIsRed = robot.leftColorRed();
+                    if (colorIsRed) {
+                        robot.driveArcade(-0.25, 0);
+                    } else {
+                        robot.driveArcade(0.25, 0);
+                    }
+                } else if (elapsedTime.seconds() < 3) {
+                    robot.rightJewelServo.setPosition(robot.RIGHT_RELEASE_POSITION);
+                } else if (((robot.leftMotor.getCurrentPosition() + robot.rightMotor.getCurrentPosition()) / 2 < 32 * COUNTS_PER_INCH)) {
+                    robot.driveArcade(0.25, 0);
+                } else {
+                    robot.driveArcade(0, 0);
+                    currentStep++;
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+
+        }
+        if (elapsedTime.seconds() < 1.0)
+
+        {
             robot.grip(Robot.GRIPPER_STATES.GRIPPER_FULL_GRIP);
             telemetry.addData("Status", "Gripping");
-        } else if (elapsedTime.seconds() < 2.0) {
+        } else if (elapsedTime.seconds() < 2.0)
+
+        {
             robot.driveArcade(0.5, 0);
             telemetry.addData("Status", "Driving");
-        } else {
+        } else
+
+        {
             robot.driveArcade(0.0, 0);
             telemetry.addData("Status", "Done");
         }
